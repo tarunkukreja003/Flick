4fed46fffbb5e44245c8c86565409e96b030353f
Tarun Kukreja <tk15797@outlook.com>
2017-03-19 01:00:43 +0530

    Splash Screen added

---
+++ app/src/main/java/com/example/tarunkukreja/moviesdb/HomeActivity.java
@@ -0,0 +1,10 @@
+package com.example.tarunkukreja.moviesdb;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by tarunkukreja on 18/03/17.
+ */
+
+public class HomeActivity extends AppCompatActivity {
+}
